1a5512acca5bacf72a2c2c192f5adc930f854c88
aguinaldo1 <aguinaldoamerico2@gmail.com>
2026-09-08 14:08:10 -0300

    feat: add sample application

---
+++ app/src/main/java/com/aguinaldo/reliabilityapi/ReliabilityApiApplication.java
@@ -0,0 +1,13 @@
+package com.aguinaldo.reliabilityapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReliabilityApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReliabilityApiApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/aguinaldo/reliabilityapi/ReliabilityApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguinaldo.reliabilityapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReliabilityApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
