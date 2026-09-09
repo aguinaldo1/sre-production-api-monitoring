08c1b69e89f6cee6a76cfe97891c2e90da9ab1cb
aguinaldo1 <aguinaldoamerico2@gmail.com>
2026-09-09 17:31:16 -0300

    feat: add controlled error endpoint

---
+++ app/src/main/java/com/aguinaldo/reliabilityapi/ErrorController.java
@@ -0,0 +1,16 @@
+package com.aguinaldo.reliabilityapi;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ErrorController {
+
+    @GetMapping("/error-test")
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String errorTest() {
+        return "Simulated internal server error";
+    }
+}
