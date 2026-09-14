59b5fc361e8606da501395d579cf3498344e3806
aguinaldo1 <aguinaldoamerico2@gmail.com>
2026-09-14 11:39:18 -0300

    refactor: organize error controller package

--- app/src/main/java/com/aguinaldo/reliabilityapi/controller/ErrorController.java
+++ app/src/main/java/com/aguinaldo/reliabilityapi/controller/ErrorController.java
@@ -1,4 +1,4 @@
-package com.aguinaldo.reliabilityapi;
+package com.aguinaldo.reliabilityapi.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
